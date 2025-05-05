f13a262a0238597184c13bf8f6c8a5d1b9eaa87c
ManuelCasanuova <manuelcasanuova@gmail.com>
2025-05-05 11:47:05 +0200

    Creazione classe Cliente con Enum

---
+++ src/test/java/it/epicode/bw/finale/clienti/Cliente.java
@@ -0,0 +1,55 @@
+package it.epicode.bw.finale.clienti;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "clienti")
+
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 100)
+    private String ragioneSociale;
+
+    @Column(unique = true, nullable = false, length = 100)
+    private String partitaIva;
+
+    @Column(unique = true, nullable = false, length = 100)
+    private String email;
+
+    private LocalDate dataInserimento;
+    private LocalDate dataUltimoContatto;
+    private double fatturatoAnnuale;
+
+    @Column(unique = true, nullable = false, length = 100)
+    private String pec;
+    private String telefono;
+
+    @Column(length = 100)
+    private String nomeContatto;
+    @Column(length = 100)
+    private String cognomeContatto;
+
+    @Column(unique = true, length = 100)
+    private String emailContatto;
+
+    private String telefonoContatto;
+
+    private String logoAziendale;
+
+    //private Indirizzi indirizzi;
+
+    @Enumerated(EnumType.STRING)
+    private TipoCliente tipoCliente;
+
+}
