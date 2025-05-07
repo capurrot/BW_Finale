868b94acb51c0bd13d147012c64e52e99347c0ba
MeridjanShoti <meridjanshoti@gmail.com>
2025-05-07 16:29:06 +0200

    test authcontroller funzionanti

--- src/main/java/it/epicode/bw/finale/auth/LoginRequest.java
+++ src/main/java/it/epicode/bw/finale/auth/LoginRequest.java
@@ -1,8 +1,12 @@
 package it.epicode.bw.finale.auth;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginRequest {
     private String username;
     private String password;
--- src/test/java/it/epicode/bw/finale/AuthControllerTest.java
+++ src/test/java/it/epicode/bw/finale/AuthControllerTest.java
@@ -1,18 +1,21 @@
 package it.epicode.bw.finale;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import it.epicode.bw.finale.auth.RegisterRequest;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import it.epicode.bw.finale.auth.*;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Set;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureTestDatabase
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class AuthControllerTest {
     @LocalServerPort
     public int port;
@@ -20,34 +23,74 @@ public class AuthControllerTest {
     private TestRestTemplate restTemplate;
     @Autowired
     private ObjectMapper objectMapper;
-    private static String token;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private AppUserRepository appUserRepository;
+    private static String tokenNormale;
+    private static String tokenAdmin;
 
     @Test
+    @Order(2)
     @DisplayName("Test di registrazione utente")
     public void testRegisterUser() {
         RegisterRequest registerRequest = new RegisterRequest("giorgio.mastrota", "password", "mondialcasa@example.com", "Giorgio", "Mastrota");
         ResponseEntity<String> response = restTemplate.postForEntity("http://localhost:"+ port +"/register", registerRequest, String.class);
         assertThat(response.getStatusCodeValue()).isEqualTo(200);
     }
-    /*@Test
+    @Test
+    @Order(1)
     @DisplayName("Test di login utente")
     public void testLoginUser() {
-        // Implementa il test per il login di un utente
+        AppUser appUser = new AppUser();
+        appUser.setUsername("gianni.bugno");
+        appUser.setPassword(passwordEncoder.encode("password"));
+        appUser.setRoles(Set.of(Role.ROLE_USER));
+        appUserRepository.save(appUser);
+        LoginRequest loginRequest = new LoginRequest("gianni.bugno", "password");
+        ResponseEntity<String> response = restTemplate.postForEntity("http://localhost:"+ port +"/login", loginRequest, String.class);
+        tokenNormale = response.getBody();
+        assertThat(response.getStatusCodeValue()).isEqualTo(200);
     }
     @Test
+    @Order(3)
     @DisplayName("Test di registrazione admin")
     public void testRegisterAdmin() {
-        // Implementa il test per la registrazione di un admin
+        //creo un admin
+        AppUser appUser = new AppUser();
+        appUser.setUsername("admin.test");
+        appUser.setPassword(passwordEncoder.encode("adminpwd"));
+        appUser.setRoles(Set.of(Role.ROLE_ADMIN));
+        appUserRepository.save(appUser);
+
+        //accedo con l'admin appena creato
+        LoginRequest loginRequest = new LoginRequest("admin.test", "adminpwd");
+        ResponseEntity<AuthResponse> response = restTemplate.postForEntity("http://localhost:"+ port +"/login", loginRequest, AuthResponse.class);
+        tokenAdmin = response.getBody().getToken();
+        //creo un admin dal controller
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setBearerAuth(tokenAdmin);
+        RegisterRequest registerRequest = new RegisterRequest("admin.test2", "password", "admin@example.com", "Admin", "Test");
+        HttpEntity<RegisterRequest> entity = new HttpEntity<>(registerRequest, headers);
+        ResponseEntity<String> response2 = restTemplate.postForEntity("http://localhost:"+ port +"/register-admin", entity, String.class);
+        assertThat(response2.getStatusCodeValue()).isEqualTo(200);
     }
     @Test
+    @Order(4)
     @DisplayName("Test di recupero utente corrente")
     public void testGetCurrentUser() {
-        // Implementa il test per ottenere l'utente corrente
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setBearerAuth(tokenAdmin);
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+        ResponseEntity<String> response = restTemplate.exchange("http://localhost:"+ port +"/current-user", HttpMethod.GET , entity ,String.class);
+        assertThat(response.getStatusCodeValue()).isEqualTo(200);
+        assertThat(response.getBody()).contains("admin.test");
     }
-    @Test
-    @DisplayName("test test")
-    public void test() {
-        assertThat(1).isEqualTo(1);
-    }*/
+
+
+
+
 
 }
