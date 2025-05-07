e9e32e758710940a16465d48298ebabc41dae78a
MeridjanShoti <meridjanshoti@gmail.com>
2025-05-07 10:08:39 +0200

    test

---
+++ src/test/java/it/epicode/bw/finale/AuthControllerTest.java
@@ -0,0 +1,10 @@
+package it.epicode.bw.finale;
+
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class AuthControllerTest {
+    
+}
